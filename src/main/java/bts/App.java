73614fbc0b718cb5a2f7b44e0ce3b07376c30406
Sajida <sajidauop@gmail.com>
2025-06-08 15:34:06 -0500

    initial commit

---
+++ src/main/java/bts/App.java
@@ -0,0 +1,22 @@
+package bts;
+
+/*import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+*/
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public String message() {
+        return "welcome to my first DevOps from javatechie";
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! testing under new package bts");
+        //SpringApplication.run(App.class,args);
+    }
+}
